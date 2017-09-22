76d04d2d2db9c547786f8f3a640de238048dbbc7
Mariam Marzouk <mmarzouk3@gatech.edu>
2017-09-22 14:14:00 -0400

    Add cancel button

--- app/src/main/java/cs2340/nycratsightings/LoginActivity.java
+++ app/src/main/java/cs2340/nycratsightings/LoginActivity.java
@@ -1,7 +1,11 @@
 package cs2340.nycratsightings;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,14 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        final Button login = (Button) findViewById(R.id.login);
+        login.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
