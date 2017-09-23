8a6b908e028fea440503f5041c70203d7b84c770
Alexander Zhang <Alex@mbp.attlocal.net>
2017-09-23 16:34:55 -0400

    Edit main page to include logout function

--- app/src/main/java/cs2340/nycratsightings/MainActivity.java
+++ app/src/main/java/cs2340/nycratsightings/MainActivity.java
@@ -1,13 +1,37 @@
 package cs2340.nycratsightings;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mlogoutText;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mlogoutText = (TextView) findViewById(R.id.logoutText);
+        mlogoutText.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.logoutText:
+                i = new Intent(this, WelcomeActivity.class);
+                this.startActivity(i);
+                break;
+            default:
+                break;
+        }
+
     }
 }
