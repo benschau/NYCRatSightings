f6c40ba9a391dad8acfe9f444b8cee15a9170caa
lucas <lucasliu@gatech.edu>
2017-09-24 18:04:59 -0400

    added functionality to all login buttons, added a user class, added mainActivity to manifest

--- app/src/main/java/cs2340/nycratsightings/LoginActivity.java
+++ app/src/main/java/cs2340/nycratsightings/LoginActivity.java
@@ -6,21 +6,65 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        final Button login = (Button) findViewById(R.id.login);
-        login.setOnClickListener(new View.OnClickListener() {
+        //Creating dummy user here
+        final  user dummy = new user("lmao","dank");
 
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, WelcomeActivity.class);
-                startActivity(intent);
+        final Button login = (Button) findViewById(R.id.login);
+        final Button welcomeReturnButton = (Button) findViewById(R.id.returnToWelcomeButton);
+        final TextView registerHere = (TextView) findViewById(R.id.signup);
+        registerHere.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                goToRegister(v);
             }
         });
+
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String emaildata = ((EditText)findViewById(R.id.email)).getText().toString();
+                String password = ((EditText)findViewById(R.id.password)).getText().toString();
+                if(dummy.validateLogin(emaildata,password)){
+                    goToMain(v);
+                }
+            }
+        });
+        welcomeReturnButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                returnToWelcome(v);
+            }
+        });
+    }
+    public void returnToWelcome(View view) {
+        startActivity(new Intent(view.getContext(),WelcomeActivity.class));
+    }
+    public void goToMain(View view){
+        startActivity(new Intent(view.getContext(),MainActivity.class));
+    }
+    public void goToRegister(View view){
+        startActivity(new Intent(view.getContext(),RegisterActivity.class));
+    }
+    //User object
+    class user{
+        private String username;
+        private String password;
+        public user(String userNameIn, String passwordIn){
+            username = userNameIn;
+            password = passwordIn;
+        }
+        public boolean validateLogin(String nameTest, String passwordTest){
+            if(username.equals(nameTest) && password.equals(passwordTest)){
+                return true;
+            } else{
+                return false;
+            }
+        }
     }
 }
