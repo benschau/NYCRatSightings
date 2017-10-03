b388f608e22702acea699aa52c758a20a43da0cf
gerardoprada <gpradag@msn.com>
2017-10-02 23:56:29 -0400

    Check that fields are completed when registering

--- app/src/main/java/cs2340/nycratsightings/RegisterActivity.java
+++ app/src/main/java/cs2340/nycratsightings/RegisterActivity.java
@@ -2,9 +2,8 @@ package cs2340.nycratsightings;
 
 import android.content.Context;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -24,22 +23,30 @@ public class RegisterActivity extends AppCompatActivity {
 
         registerButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                Context context;
                 String email, password;
 
                 email = ((EditText) findViewById(R.id.email)).getText().toString();
                 password = ((EditText) findViewById(R.id.password)).getText().toString();
 
-                // Get selected user type from registration screen and create corresponding user.
+                // Checks if all fields have been completed.
                 int selectedUserTypeId = userType.getCheckedRadioButtonId();
-                RadioButton selectedRB = (RadioButton) findViewById(selectedUserTypeId);
-                String selectedUserType = selectedRB.getText().toString();
-                
-                if (selectedUserType.equals("Administrator")) {
-                    Admin newAdminUser = new Admin(email, password);
-                    returnToWelcome(v);
+                if (!email.isEmpty() && !password.isEmpty() && selectedUserTypeId != -1) {
+                    RadioButton selectedRB = (RadioButton) findViewById(selectedUserTypeId);
+                    String selectedUserType = selectedRB.getText().toString();
+                    if (selectedUserType.equals("Administrator")) {
+                        Admin newAdminUser = new Admin(email, password);
+                        returnToWelcome(v);
+                    } else {
+                        User newUser = new User(email, password);
+                        returnToWelcome(v);
+                    }
                 } else {
-                    User newUser = new User(email, password);
-                    returnToWelcome(v);
+                    context = getApplicationContext();
+                    CharSequence text = "Fill out all the fields to complete registration";
+                    int duration = Toast.LENGTH_SHORT;
+
+                    Toast.makeText(context, text, duration).show();
                 }
             }
         });
